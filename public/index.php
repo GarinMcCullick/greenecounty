aaa2b12b091a9c25b2247703d91899ca1b30aaf4
unknown <glmccullick@gmail.com>
2025-09-09 07:51:24 -0500

    fixed pregmatch delimiter for mock api

--- public/index.php
+++ public/index.php
@@ -1,13 +1,15 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
 
-if (preg_match('#^/greenecounty/public/api/form#', $request)) {
+//mock API Endpoint this would hit FormController()
+if (preg_match('#api/form#', $request)) {
     require __DIR__ . '/../app/controllers/FormController.php';
     $controller = new FormController();
     $controller->index();
     exit;
 }
 
+// Serve Angular app for all other routes this way we only do api endpoint routing in php and angular will do routing for pages / app navigation since its a SPA
 $indexFile = __DIR__ . '/angular/index.html';
 if (file_exists($indexFile)) {
     echo file_get_contents($indexFile);
